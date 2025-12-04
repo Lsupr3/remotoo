c8309e06208d83b020a5cff0cbcd7654078d4698
Lenin <lberiguetteramires@gmail.com>
2025-12-04 13:18:44 +0100

    prueba de modificacion de controladores

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,32 @@ public class Main {
         boolean salir = false;
 
 
+        try {
 
-        while(!salir){
-        System.out.println("cuntos popochos tiene en la cama");
-        var popo = sc.nextInt();
 
-        if (popo == 0){
-            System.out.println("conseguir uno");
-        } else if (popo == 1 ) {
-            System.out.println("ese popocho esta mimado");
-        } else if (popo == 2 || popo == 3) {
-            System.out.println("la familia de ese popocho esta equilibrado");
-        } else if (popo == 3) {
-            System.out.println("son demaciados popochos no hay espacio en la cama");
-        }
-            for (int i = 0; i <popo ; i++) {
-                System.out.println("Zzz" + i++);
-                System.out.println("todos los popochitos estan durmirndo");
+            while (!salir) {
+                System.out.println("cuntos popochos tiene en la cama");
+                var popo = sc.nextInt();
+
+                if (popo == 0) {
+                    System.out.println("conseguir uno");
+                } else if (popo == 1) {
+                    System.out.println("ese popocho esta mimado");
+                } else if (popo == 2 || popo == 3) {
+                    System.out.println("la familia de ese popocho esta equilibrado");
+                } else if (popo == 3) {
+                    System.out.println("son demaciados popochos no hay espacio en la cama");
+                }
+                for (int i = 0; i < popo; i++) {
+                    System.out.println("Zzz" + i++);
+                    System.out.println("todos los popochitos estan durmiendo");
+                }
             }
+            System.out.println("proyecto modificado por otros programadores 04/12/2025");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
-        }
-
-        }
-
+    }
+}
